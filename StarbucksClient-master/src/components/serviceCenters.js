3c0cbeb3eb0f360507fdf0e26306e02e8df117db
JiaMa-SJSU <47645194+JiaMa-SJSU@users.noreply.github.com>
2019-05-10 15:05:29 -0700

    update google map api

---
+++ StarbucksClient-master/src/components/serviceCenters.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+export default [
+    {
+    "name": "Mission Collage Starbucks",
+    "address": "3113 Mission College Blvd Bldg 5",
+    "city": "Santa Clara",
+    "state": "CA",
+    "zip_code": "95054",
+    "country": "US",
+    "lat": "37.3891122",
+    "lng": "-121.9856487",
+    "phone_number": "408-727-1419",
+    "fax_number": null
+    },
+    {
+    "name": "Starbucks",
+    "address": "2401 Agnew Rd, Santa Clara, CA 95054",
+    "city": "Santa Clara",
+    "state": "CA",
+    "zip_code": "95054",
+    "country": "US",
+    "lat": "37.3891332",
+    "lng": "-121.9922148",
+    "phone_number": "877-421-9062",
+    "fax_number": null
+    }, 
+    {
+    "name": "Starbucks",
+    "address": "3253 Coronado Place",
+    "city": "Santa Clara",
+    "state": "CA",
+    "zip_code": "95054",
+    "country": "US",
+    "lat": "37.3895295",
+    "lng": "-121.9940836",
+    "phone_number": "408-988-8776",
+    "fax_number": null
+    }
+    ]
